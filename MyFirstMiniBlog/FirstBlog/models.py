6887cd611a411f000b8c6d892019bfd67ae542dd
rezny <reznitchenko1505@gmail.com>
2024-01-13 10:30:11 +0200

    2nd lesson

--- MyFirstMiniBlog/FirstBlog/models.py
+++ MyFirstMiniBlog/FirstBlog/models.py
@@ -1,3 +1,18 @@
+from audioop import reverse
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    '''note data'''
+    title = models.CharField('Topic', max_length=100)
+    description = models.TextField('Text of the note')
+    author = models.CharField('Author', max_length=100)
+    date = models.DateField('Date')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes'
+
